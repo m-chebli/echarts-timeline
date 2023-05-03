b788d0c71144b92029d6fad44e3f95ca35cef542
Manfred <m.chebli@ateme.com>
2023-05-03 10:48:33 +0200

    example code

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,30 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import TimelineEchart from './components/TimelineEchart.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="common-layout">
+    <el-container>
+      <el-main>
+        <el-row class="el-row" :gutter="12">
+          <el-col :span="24">
+            <el-card class="no-v-padding">
+              <TimelineEchart />
+            </el-card>
+          </el-col>
+        </el-row>
+      </el-main>
+    </el-container>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.no-v-padding {
+  padding-right: 0;
+  padding-left: 0;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.el-row {
+  margin-bottom: 10px;
 }
 </style>
